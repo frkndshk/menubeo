4258e1d596188fc424493918e9d2cc9493381c63
frkndshk <frknsdhk@gmail.com>
2024-01-02 19:01:40 +0300

    buton eklemek

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridGap: '10px' }}>
+                    <button>Buton 1</button>
+                    <button>Buton 2</button>
+                    <button>Buton 3</button>
+                    <button>Buton 4</button>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
